2f1ef4c53d36e9b17738cf2ab1467d2cb6ac3ae6
Scott Graham <scottmg@chromium.org>
2017-04-15 13:37:34 -0700

    Fix build warning on VS2015+ and rebuild with VS2017
    Fixes #1.

--- m.bat
+++ m.bat
@@ -1,2 +1,2 @@
-@cl /W4 /WX /nologo /Ox /MT /DUNICODE /D_UNICODE tim.cc /link shell32.lib /out:tim.exe
+@cl /W4 /WX /nologo /Ox /EHsc /MT /DUNICODE /D_UNICODE tim.cc /link shell32.lib /out:tim.exe
 @copy tim.exe timavg.exe
